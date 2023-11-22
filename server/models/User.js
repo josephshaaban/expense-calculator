e921b56a00065688d1e34b6c06fa84aa46ec32b6
Youssef Shaaban <joseph.sha3ban@gmail.com>
2023-11-22 14:05:22 +0400

    [SERVER]: adds some helper methods to User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // User schema
 const userSchema = new Schema(
@@ -32,6 +33,21 @@ const userSchema = new Schema(
     toJSON: { virtuals: true },
   },
 );
+
+// Hash user password
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// Custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 // User model
 const User = model("User", userSchema);
