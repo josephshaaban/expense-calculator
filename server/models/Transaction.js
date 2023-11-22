a92efb3d28c5d6cb6edc6df1f510fc1cb16a09cb
Youssef Shaaban <joseph.sha3ban@gmail.com>
2023-11-22 13:52:52 +0400

    [SERVER]: adds Transaction model

---
+++ server/models/Transaction.js
@@ -0,0 +1,64 @@
+const { Schema, model } = require("mongoose");
+
+// Transaction schema
+const transactionSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    transactionType: {
+      type: String,
+      enum: ["Income", "Expenses"],
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Food",
+        "Transportation",
+        "Entertainment",
+        "Shopping",
+        "Utilities",
+        "Health",
+        "Travel",
+        "Education",
+        "Personal",
+        "Groceries",
+        "Bills",
+        "Uncategorized",
+      ],
+      required: true,
+    },
+    color: {
+      type: String,
+    },
+    //transaction will created by a user so using "Referencing"
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now(),
+    },
+    notes: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  },
+);
+
+// Transaction model
+const Transaction = model("Transaction", transactionSchema);
+
+module.exports = Transaction;
---
+++ server/models/index.js
@@ -0,0 +1,5 @@
+const User = require("./User");
+const Account = require("./Account");
+const Transaction = require("./Transaction");
+
+module.exports = { User, Account, Transaction };
