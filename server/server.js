b6706c0087cebdaef516f0baa298af7c0a8f4fb6
Youssef Shaaban <joseph.sha3ban@gmail.com>
2023-11-22 14:05:47 +0400

    [SERVER]: fix typo

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const path = require("path");
 require("dotenv").config({ path: "./config.env" });
 const PORT = process.env.PORT || 9000;
 const API_VERSION = process.env.API_VERSION || "v1";
-const BASE_URL = process.env.BASE_URL || `/api/${API_VERSION}`;
+const API_URI = process.env.API_URI || `/api/${API_VERSION}`;
 
 // Middlewares
 app.use(express.json()); //parse incoming data
@@ -20,7 +20,7 @@ const conn = require("./config/connection");
 
 // Error handling
 
-// Listen to server
+// Connect to MongoDB and then listen to server
 conn
   .then((db) => {
     if (!db) return process.exit(1);
@@ -32,7 +32,7 @@ conn
         } else {
           console.log(`API server running on port ${PORT}!`);
           console.log(
-            `Use expense calculator API at http://localhost:${PORT}${BASE_URL}`,
+            `Use expense calculator API at http://localhost:${PORT}${API_URI}`,
           );
         }
       });
